4e909a384b676452e6e91c3ff59a52e07b6cc946
Shankar Emani <94682274+Shankaremani@users.noreply.github.com>
2022-01-31 05:49:47 +0000

    .

--- p1original.c
+++ p1original.c
@@ -1,2 +1,15 @@
-
+#include<stdio.h>
+void input(int *a,int *b)
+{
+  int a,b;
+  printf("enter two numbers\n");
+  scanf("%d%d",a,b);
+}
+void add(int a,int b,int*sum);
+{
+  *sum=a+b;
+}
+void output(int a,int b,int sum)
+{
+  printf("sum of %d ")
 }
